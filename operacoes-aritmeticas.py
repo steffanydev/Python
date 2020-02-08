8eefcc13e867fd0086d86c409dc756466335c581
Stéffany Caroline <steffany.dev@outlook.com>
2020-02-07 22:24:04 -0300

    Operações artméticas

---
+++ operacoes-aritmeticas.py
@@ -0,0 +1,8 @@
+# Operações aritméticas
+v1 = int(input('Digite o primeiro valor: '))
+v2 = int(input('Digite o segundo valor: '))
+print(f'O resultado da soma é {v1 + v2}')
+print(f'O resultado da subtração é {v1 - v2}')
+print(f'O resultado da multiplicação é {v1 * v2}')
+print(f'O resultado da divisão é {v1 / v2}')
+print(f'O resultado da potência é {v1 ** v2}')
