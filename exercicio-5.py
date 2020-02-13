e5a3f0ac494eeb51111c69600d87bb571fe163a6
Stéffany Caroline <steffany.dev@outlook.com>
2020-02-12 22:04:11 -0300

    Script que leia um valor em metros e exiba convertido em centímetros e milímetros

---
+++ exercicio-5.py
@@ -0,0 +1,4 @@
+metro = float(input('Digite um valor: '))
+print('Metros {:.0f}m'.format(metro))
+print('Centímetros {:.0f}cm'.format(metro * 100))
+print('Milímetros {:.0f}mm'.format(metro * 1000))
