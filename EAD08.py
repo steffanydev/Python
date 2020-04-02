c152fec84941100b6b2efaa6effed0ac4d827f15
Stéffany Caroline <steffany.dev@outlook.com>
2020-04-01 22:25:57 -0300

    Aula EAD 01/04

---
+++ EAD08.py
@@ -0,0 +1,8 @@
+numero = int(input('Digite um número: '))
+x = 0
+while (x <= 10):
+    # Multiplicação do número
+    mult = x * numero
+    print('{} X {} = {}'.format(x, numero, mult))
+    # Somando mais 1
+    x = x + 1
